090862f58bebe6608e07094e25849a2d6e3035c2
Saransh Raina <saranshraina1@gmail.com>
2023-08-14 22:02:30 -0500

    order of components changed for rendering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,33 +3,37 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Portfolio from './pages/Portfolio'
 import Contact from './pages/Contact'
+import Navbar from './components/Navbar'
 import './App.css';
 
 const App = () => {
   const [dots, setDots] = useState([]);
+  const [showDots, setShowDots] = useState(true)
 
   useEffect(() => {
     let animationFrameId;
     const animate = () => {
-      setDots((prevDots) => prevDots.map((dot) => {
-        let newVX = dot.vx;
-        let newVY = dot.vy;
-        if (dot.x < 0 || dot.x >= window.innerWidth) {
-          newVX = -dot.vx;
-        }
-        if (dot.y < 0 || dot.y >= window.innerHeight) {
-          newVY = -dot.vy;
-        }
+      if (showDots) {
+        setDots((prevDots) => prevDots.map((dot) => {
+          let newVX = dot.vx;
+          let newVY = dot.vy;
+          if (dot.x < 0 || dot.x >= window.innerWidth) {
+            newVX = -dot.vx;
+          }
+          if (dot.y < 0 || dot.y >= window.innerHeight) {
+            newVY = -dot.vy;
+          }
 
-        return {
-          ...dot,
-          x: dot.x + newVX,
-          y: dot.y + newVY,
-          vx: newVX,
-          vy: newVY,
-        };
-      })
-    );
+          return {
+            ...dot,
+            x: dot.x + newVX,
+            y: dot.y + newVY,
+            vx: newVX,
+            vy: newVY,
+            };
+          })
+        );
+      }
       animationFrameId = requestAnimationFrame(animate);
     };
 
@@ -37,7 +41,7 @@ const App = () => {
     return () => {
       cancelAnimationFrame(animationFrameId);
     };
-  }, []);
+  }, [showDots]);
 
   const getRandomColor = () => {
     const r = Math.floor(Math.random() * 256);
@@ -61,15 +65,12 @@ const App = () => {
     setDots([...dots, newDot]);
   };
 
+  const handleHideBackground = () => {
+    setShowDots(!showDots);
+  };
+
   return (
-    <div className="App" onClick={handleDotClick}>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/Portfolio' element={<Portfolio/>}/>
-          <Route path='/Contact' element={<Contact/>}/>
-        </Routes>   
-      </Router>
+    <div className='App' onClick={handleDotClick}>
       {dots.map((dot, index) => (
         <div
           key={index}
@@ -79,22 +80,33 @@ const App = () => {
           {dots.map((otherDot, otherIndex) => {
             if (index !== otherIndex) {
               const distance = Math.sqrt((dot.x - otherDot.x) ** 2 + (dot.y - otherDot.y) ** 2)
-              if (distance <= 100) {
-                return (
-                  <div
-                    key={otherIndex}
-                    className="line"
-                    style={{left: 6, top: 6, width: distance,
-                      transform: `rotate(${Math.atan2(otherDot.y - dot.y, otherDot.x - dot.x)}rad)`
-                    }}
-                  ></div>
-                );
-              }
+              if (distance <= 80) {
+                  return (
+                    <div
+                      key={otherIndex}
+                      className="line"
+                      style={{left: 4, top: 4, width: distance,
+                        transform: `rotate(${Math.atan2(otherDot.y - dot.y, otherDot.x - dot.x)}rad)`
+                      }}
+                    ></div>
+                  );
+                }
             }
             return null;
           })}
         </div>
       ))}
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/Portfolio' element={<Portfolio/>}/>
+          <Route path='/Contact' element={<Contact/>}/>
+        </Routes>
+        <Navbar/>   
+      </Router>
+      <button className='hide-background-button' onClick={handleHideBackground}>
+        {showDots ? 'Pause Background' : 'Resume Background'}
+      </button>
     </div>
   );
 }
