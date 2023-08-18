99b84508bf33fb28c287e9fbc0fca43e10f3cdaf
Saransh Raina <saranshraina1@gmail.com>
2023-08-18 17:32:00 -0500

    contact page will be done later

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom';
 
+import './Contact.css'
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact-container'>
+      <div className="cont-buttons-container">
+        <Link to="/"><button className="cont-home-button">Home</button></Link>
+        <Link to="/Portfolio"><button className="cont-portfolio-button">Portfolio</button></Link>
+      </div>
+      <h1 className='header-text'>Currently Under Construction</h1>
+    </div>
   )
 }
 
