7fb62db3a716b9a96ac3c138dea606a6304cb0b9
sr52767 <sraina1@utexas.edu>
2024-05-28 10:36:45 -0500

    contact page frontend

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,14 +1,74 @@
 import { Link } from 'react-router-dom';
-
+import { useState } from 'react'
 import './Contact.css'
+
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:8080/api/contact', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData)
+      });
+      if (response.ok) {
+        alert('Message sent successfully!');
+        setFormData({
+          name: '',
+          email: '',
+          message: ''
+        });
+      } else {
+        alert('Failed to send message.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      alert('Failed to send message.');
+    }
+  };
+
   return (
     <div className='contact-container'>
       <div className="cont-buttons-container">
         <Link to="/"><button className="cont-home-button">Home</button></Link>
         <Link to="/Portfolio"><button className="cont-portfolio-button">Portfolio</button></Link>
       </div>
-      <h1 className='header-text'>Currently Under Construction</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name:</label>
+          <input type="text" name="name" value={formData.name} onChange={handleChange} required/>
+        </div>
+        <div>
+          <label>Email:</label>
+          <input type="email" name="email" value={formData.email} onChange={handleChange} required/>
+        </div>
+        <div>
+          <label>Subject:</label>
+          <textarea name="subject" value={formData.subject} onChange={handleChange} required/>
+        </div>
+        <div>
+          <label>Message:</label>
+          <textarea name="message" value={formData.message} onChange={handleChange} required/>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   )
 }
