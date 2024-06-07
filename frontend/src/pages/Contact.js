086e6459648cd103b19d35458ff3fc8e5dc08878
sr52767 <sraina1@utexas.edu>
2024-06-06 21:50:08 -0400

    final contact page

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -21,7 +21,7 @@ const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:8080/api/contact', {
+      const response = await fetch('https://srwebsite-contact.onrender.com/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
